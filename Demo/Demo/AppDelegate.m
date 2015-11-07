7f5d2f5d3a9b63ee575b9d8a5ffe8468a52ed263
yuwei870306 <yuwei870306@163.com>
2015-11-07 17:27:54 +0800

    wss

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"lcy");
+    NSString *sssssssssss = nil;
     return YES;
 }
 
